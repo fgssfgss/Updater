c891552d1c4f1fd84ec1f9924b8d9258070470ae
fgssfgss <andrew612.ru@gmail.com>
2015-05-14 20:25:35 +0300

    Config parser added, changes in obfuscation algorithm.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import static java.sql.DriverManager.getConnection;
@@ -13,6 +11,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -20,14 +24,78 @@ import java.util.regex.Pattern;
  */
 public class Main {
 
-    public String url = "jdbc:mysql://";
+    private class ConfigParser {
+
+        private Element root;
+
+        public ConfigParser(String filename) {
+            try {
+                File file = new File(filename);
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document document = builder.parse(file);
+                root = document.getDocumentElement();
+
+            } catch (ParserConfigurationException | SAXException | IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        public String getUrl() {
+            String result = "jdbc:mysql://";
+            Element elem = (Element) root.getElementsByTagName("host").item(0);
+            result = result.concat(elem.getTextContent());
+            elem = (Element) root.getElementsByTagName("port").item(0);
+            result = result.concat(":" + elem.getTextContent());
+            elem = (Element) root.getElementsByTagName("db_name").item(0);
+            result = result.concat("/" + elem.getTextContent());
+            return result;
+        }
+
+        public String getTable() {
+            Element elem = (Element) root.getElementsByTagName("table").item(0);
+            return elem.getTextContent();
+        }
+
+        public String getUserName() {
+            Element elem = (Element) root.getElementsByTagName("user").item(0);
+            return elem.getTextContent();
+        }
+
+        public String getPassword() {
+            Element elem = (Element) root.getElementsByTagName("password").item(0);
+            return elem.getTextContent();
+        }
+
+        public String getColumn1() {
+            Element elem = (Element) root.getElementsByTagName("column1").item(0);
+            return elem.getTextContent();
+        }
+
+        public String getColumn2() {
+            Element elem = (Element) root.getElementsByTagName("column2").item(0);
+            return elem.getTextContent();
+        }
+
+        public String getFiller() {
+            Element elem = (Element) root.getElementsByTagName("filler").item(0);
+            return elem.getTextContent();
+        }
+
+        public String getCondition() {
+            Element elem = (Element) root.getElementsByTagName("condition").item(0);
+            return elem.getTextContent();
+        }
+    }
+
+    public String url = "";
     public String table = "";
-    public String user = "root";
-    public String password = "toor";
-    public String column1 = "text1";
-    public String column2 = "text2";
+    public String user = "";
+    public String password = "";
+    public String column1 = "";
+    public String column2 = "";
     public String filler = "";
-    public String fillerfilename = "";
+    public String condition = "";
     public String regexp_phone = "\\+?(\\d{12})";
 
     public static void main(String[] args) {
@@ -35,31 +103,22 @@ public class Main {
     }
 
     public void run(String[] args) {
-        if(args.length < 6)
-        {
-            System.out.println("Usage: java -jar utility.jar host:port db_name table username password column1 column2 filler.txt");
+        if (args.length < 1) {
+            System.out.println("Usage: java -jar utility.jar config.xml");
             System.exit(1);
         }
-        
-        url = url.concat(args[0] + "/" + args[1]); // arg 0 for host:port, arg 1 for db
-        table = args[2]; // arg 2 for table
-        user = args[3]; //  arg 3 for username
-        password = args[4]; // arg 4 for password
-        column1 = args[5]; // arg 5 for column with text
-        column2 = args[6]; // arg 6 for column with phone number
-        fillerfilename = args[7]; // arg 7 for filler.txt
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(fillerfilename));
-            String tmp;
-            while((tmp = in.readLine()) != null){
-                filler = filler.concat(tmp);
-            }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+
+        ConfigParser cp = new ConfigParser(args[0]);
+
+        url = cp.getUrl();
+        table = cp.getTable();
+        user = cp.getUserName();
+        password = cp.getPassword();
+        column1 = cp.getColumn1();
+        column2 = cp.getColumn2();
+        filler = cp.getFiller();
+        condition = cp.getCondition();
+
         try {
             Connection con;
             Statement st;
@@ -68,7 +127,7 @@ public class Main {
             con = getConnection(url, user, password);
 
             st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            rs = st.executeQuery("SELECT * FROM " + table);
+            rs = st.executeQuery("SELECT * FROM " + table + " WHERE " + condition);
 
             while (rs.next()) {
                 String s1 = rs.getString(column1);
@@ -88,27 +147,31 @@ public class Main {
     }
 
     public String fillWithText(String text) {
+        Random rand = new Random();
         Integer fillerLength = filler.length();
         Integer textLength = text.length();
-        if (fillerLength >= textLength) {
-            return filler.substring(0, textLength);
+        Integer length = rand.nextInt(fillerLength - 5) + 1;
+        Integer beginpos = rand.nextInt(fillerLength - length);
+        String str = filler.substring(beginpos, beginpos + length);
+        if (length >= textLength) {
+            return str.substring(0, textLength);
         } else {
             String result = new String();
             do {
-                result = result.concat(filler);
-                textLength -= fillerLength;
-            } while (textLength >= fillerLength);
-            result = result.concat(filler.substring(0, textLength));
+                result = result.concat(str);
+                textLength -= length;
+            } while (textLength >= length);
+            result = result.concat(str.substring(0, textLength));
             return result;
         }
     }
 
     public String generateRandomNumber() {
-        int size = 7;
+        int size = 8;
         char[] result = new char[size];
         Random rand = new Random();
         for (int i = 0; i < size; i++) {
-            result[i] = (char)((char)rand.nextInt()% 10 + 48);
+            result[i] = (char) ((char) rand.nextInt() % 10 + 48);
         }
         return new String(result);
     }
@@ -116,7 +179,7 @@ public class Main {
     public String obfuscateNumber(String text) {
         Pattern pattern = Pattern.compile(regexp_phone);
         Matcher matcher = pattern.matcher(text);
-        String res = matcher.replaceAll("+1111"+generateRandomNumber());
+        String res = matcher.replaceAll("+1111" + generateRandomNumber());
         return res;
     }
 
