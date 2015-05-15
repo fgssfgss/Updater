149d4ef586ead034ae2f2652df88daa320fe80ba
fgssfgss <andrew612.ru@gmail.com>
2015-05-15 18:59:40 +0300

    Config file format improvement.

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -27,6 +28,7 @@ public class Main {
     private class ConfigParser {
 
         private Element root;
+        private NodeList nodeList;
 
         public ConfigParser(String filename) {
             try {
@@ -35,6 +37,7 @@ public class Main {
                 DocumentBuilder builder = factory.newDocumentBuilder();
                 Document document = builder.parse(file);
                 root = document.getDocumentElement();
+                nodeList = root.getElementsByTagName("column");
 
             } catch (ParserConfigurationException | SAXException | IOException ex) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
@@ -52,8 +55,8 @@ public class Main {
             return result;
         }
 
-        public String getTable() {
-            Element elem = (Element) root.getElementsByTagName("table").item(0);
+        public String getTable(int node) {
+            Element elem = (Element) ((Element) nodeList.item(node)).getElementsByTagName("table").item(0);
             return elem.getTextContent();
         }
 
@@ -67,35 +70,32 @@ public class Main {
             return elem.getTextContent();
         }
 
-        public String getColumn1() {
-            Element elem = (Element) root.getElementsByTagName("column1").item(0);
-            return elem.getTextContent();
-        }
-
-        public String getColumn2() {
-            Element elem = (Element) root.getElementsByTagName("column2").item(0);
-            return elem.getTextContent();
-        }
-
         public String getFiller() {
             Element elem = (Element) root.getElementsByTagName("filler").item(0);
             return elem.getTextContent();
         }
 
-        public String getCondition() {
-            Element elem = (Element) root.getElementsByTagName("condition").item(0);
+        public String getType(int node) {
+            Element elem = (Element) ((Element) nodeList.item(node)).getElementsByTagName("type").item(0);
             return elem.getTextContent();
+        }
+
+        public String getName(int node) {
+            Element elem = (Element) ((Element) nodeList.item(node)).getElementsByTagName("name").item(0);
+            return elem.getTextContent();
+        }
+
+        public String getCondition(int node) {
+            Element elem = (Element) ((Element) nodeList.item(node)).getElementsByTagName("condition").item(0);
+            return elem.getTextContent();
+        }
+
+        public Integer getLengthOfColumns() {
+            return nodeList.getLength();
         }
     }
 
-    public String url = "";
-    public String table = "";
-    public String user = "";
-    public String password = "";
-    public String column1 = "";
-    public String column2 = "";
     public String filler = "";
-    public String condition = "";
     public String regexp_phone = "\\+?(\\d{12})";
 
     public static void main(String[] args) {
@@ -109,36 +109,32 @@ public class Main {
         }
 
         ConfigParser cp = new ConfigParser(args[0]);
-
-        url = cp.getUrl();
-        table = cp.getTable();
-        user = cp.getUserName();
-        password = cp.getPassword();
-        column1 = cp.getColumn1();
-        column2 = cp.getColumn2();
         filler = cp.getFiller();
-        condition = cp.getCondition();
-
+        
         try {
             Connection con;
             Statement st;
             ResultSet rs;
 
-            con = getConnection(url, user, password);
+            con = getConnection(cp.getUrl(), cp.getUserName(), cp.getPassword());
 
             st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            rs = st.executeQuery("SELECT * FROM " + table + " WHERE " + condition);
 
-            while (rs.next()) {
-                String s1 = rs.getString(column1);
-                String s2 = rs.getString(column2);
+            for (int i = 0; i < cp.getLengthOfColumns(); i++) {
+                rs = st.executeQuery("SELECT * FROM " + cp.getTable(i) + " WHERE " + cp.getCondition(i));
 
-                s1 = fillWithText(s1);
-                s2 = obfuscateNumber(s2);
+                while (rs.next()) {
+                    String string = rs.getString(cp.getName(i));
 
-                rs.updateString(column1, s1);
-                rs.updateString(column2, s2);
-                rs.updateRow();
+                    if(cp.getType(i).equals("phone")){
+                        string = obfuscateNumber(string);
+                    } else {
+                        string = fillWithText(string);
+                    }
+
+                    rs.updateString(cp.getName(i), string);
+                    rs.updateRow();
+                }
             }
 
         } catch (SQLException ex) {
@@ -171,7 +167,7 @@ public class Main {
         char[] result = new char[size];
         Random rand = new Random();
         for (int i = 0; i < size; i++) {
-            result[i] = (char) ((char) rand.nextInt() % 10 + 48);
+            result[i] = (char) ((char) rand.nextInt(10) + 48);
         }
         return new String(result);
     }
